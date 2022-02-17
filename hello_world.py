2d55cf79e8aa87813f438b8fb91a0042e322eba6
Michael Clermont <michael.clermont@mths.ca>
2022-02-17 19:35:17 +0000

    added hello world

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Michael Clermont
+# Created on February 2022
+# This is a "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
